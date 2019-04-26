5f18762ca83e07d1edae608398e1c7af5fe9b8c9
diana pardy <dianapardy91@gmail.com>
2019-04-26 12:47:41 -0600

    Added operation test

---
+++ src/main/java/com/exams/Operation.java
@@ -0,0 +1,57 @@
+package com.exams;
+
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
+
+public enum Operation {
+    PLUS("+") {
+        public double apply(double x, double y) {
+            return x + y;
+        }
+    },
+    MINUS("-") {
+        public double apply(double x, double y) {
+            return x - y;
+        }
+    },
+    TIMES("*") {
+        public double apply(double x, double y) {
+            return x * y;
+        }
+    },
+    DIVIDE("/") {
+        public double apply(double x, double y) {
+            return x / y;
+        }
+    };
+
+    private final String operator;
+
+    Operation(String operator) {
+        this.operator = operator;
+    }
+
+    @Override
+    public String toString() {
+        return operator;
+    }
+
+    public abstract double apply(double x, double y);
+
+
+    private static final Map<String, Operation> stringToEnum =
+            Stream.of(values()).collect(
+                    toMap(Object::toString, e -> e));
+
+
+    public static Optional<Operation> fromString(String operator) {
+        return Optional.ofNullable(stringToEnum.get(operator));
+    }
+
+
+}
---
+++ src/test/java/com/exams/OperationTest.java
@@ -0,0 +1,47 @@
+package com.exams;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OperationTest {
+    @Test
+    public void operationPlus() throws Exception {
+        double leftOperand = 2;
+        double rightOperand = 3;
+        double expectedResult = leftOperand + rightOperand;
+
+        double appliedResult = Operation.PLUS.apply(leftOperand, rightOperand);
+        assertThat(appliedResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void operationMinus() throws Exception {
+        double leftOperand = 5;
+        double rightoperand = 3;
+        double expectedResult = leftOperand - rightoperand;
+
+        double appliedResult = Operation.fromString("-").get().apply(leftOperand, rightoperand);
+        assertThat(appliedResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void operationTimes() throws Exception {
+        double leftOperand = 5;
+        double rightOperand = 3;
+        double expectedResult = leftOperand * rightOperand;
+
+        double appliedResult = Operation.fromString("*").get().apply(leftOperand, rightOperand);
+        assertThat(appliedResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void operationDivide() throws Exception {
+        double leftOperand = 5;
+        double righOperand = 3;
+        double expectedResult = leftOperand / righOperand;
+
+        double appliedResult = Operation.fromString("/").get().apply(leftOperand, righOperand);
+        assertThat(appliedResult).isEqualTo(expectedResult);
+    }
+}
